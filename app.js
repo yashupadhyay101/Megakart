4d0844df9c636e564d21fe110a781491a8b2e55a
yashupadhyay101 <upadhyayyash02@gmail.com>
2022-12-28 17:38:59 +0530

    E-commerce project in NodeJs

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+//const expressHbs = require('express-handlebars');
+
+const app = express();
+
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//   })
+// );
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
